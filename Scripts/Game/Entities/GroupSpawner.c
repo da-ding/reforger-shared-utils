44fa94cf745f99fe3f9f71fb701b6d5ca1c90dd3
Michael Ruigrok <ruigrok.michael@gmail.com>
2022-07-27 23:16:01 +1000

    group spawn cleanup

--- Scripts/Game/Entities/GroupSpawner.c
+++ Scripts/Game/Entities/GroupSpawner.c
@@ -5,22 +5,18 @@ class DAD_GroupSpawnPointClass : DAD_NotPlayerSpawnPointClass
 
 class DAD_GroupSpawnPoint : DAD_NotPlayerSpawnPoint
 {
-	protected SCR_AIGroup m_Group;
-
 	void SetGroup(SCR_AIGroup group)
 	{
-		if (m_Group) m_Group.GetOnEmpty().Remove(OnGroupEmpty);
-		m_Group = group;
-		m_TargetEntity = group;
+		SCR_AIGroup oldGroup = SCR_AIGroup.Cast(GetTargetEntity());
+		if (oldGroup) oldGroup.GetOnEmpty().Remove(OnGroupEmpty);
+
 		SetEntityID(group.GetName());
-		
-		m_Group.GetOnEmpty().Insert(OnGroupEmpty);
+		group.GetOnEmpty().Insert(OnGroupEmpty);
 	}
 
 	SCR_AIGroup GetGroup()
 	{
-		/* return SCR_AIGroup.Cast(m_TargetEntity); */
-		return m_Group;
+		return SCR_AIGroup.Cast(m_TargetEntity);
 	}
 	
 	void OnGroupEmpty()
@@ -31,36 +27,21 @@ class DAD_GroupSpawnPoint : DAD_NotPlayerSpawnPoint
 	override void EOnPlayerSpawn(IEntity entity)
 	{
 		super.EOnPlayerSpawn(entity);
-		
-		if (!m_Group) return;
-
-		//SCR_AIGroup m_Group = SCR_AIGroup.Cast(m_TargetEntity);
+		SCR_AIGroup group = SCR_AIGroup.Cast(GetTargetEntity());
+		if (!group) return;
 
 		// Delete a member from the group
 		array<AIAgent> agents = new array<AIAgent>();
-		m_Group.GetAgents(agents);
+		group.GetAgents(agents);
 		int last = agents.Count() - 1;
 		IEntity ai = agents.Get(last).GetControlledEntity();
 		
 		SCR_EntityHelper.DeleteEntityAndChildren(ai);
-		m_Group.RemoveAIEntityFromGroup(ai);
+		group.RemoveAIEntityFromGroup(ai);
 
 		// TODO: How do I get the player ID out of the IEntity?
 		// Oh yeah, also, adding players to the group means that players might get yeeted
 		// if someone spawns to a group that already has players!
-		// m_Group.AddPlayer(entity);
+		// group.AddPlayer(entity);
 	}
-
-	/* protected void UpdatePosition() */
-	/* { */
-	/* 	if (!m_Group) return; */
-	/* 	SetOrigin(m_Group.GetOrigin()); */
-	/* 	Update(); */
-	/* } */
-
-	/* override void EOnInit(IEntity owner) */
-	/* { */
-	/* 	GetGame().GetCallqueue().CallLater(UpdatePosition, 2, true); */
-	/* } */
-
 };
