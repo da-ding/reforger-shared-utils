b02cac3070ef048aca9bb4b729a24e0e0109f98e
Da-ding! <109660283+da-ding@users.noreply.github.com>
2023-06-02 18:38:10 +1000

    add DAD_ReadyUpAction, for players to identify that they're ready to begin some event.

---
+++ Scripts/Game/Entities/UserActions/DAD_ReadyUpAction.c
@@ -0,0 +1,47 @@
+class DAD_ReadyUpAction : ScriptedUserAction {
+
+	protected bool ready = false;
+
+	//------------------------------------------------------------------------------------------------
+	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
+	{
+
+		this.ready = !this.ready;
+		SCR_ChimeraCharacter char = SCR_ChimeraCharacter.Cast(pUserEntity);
+		FactionKey fac = char.GetFactionKey();
+
+		if (this.ready) {
+			pOwnerEntity.SetYawPitchRoll("0 0 180");
+
+			SCR_HintManagerComponent.ShowCustomHint(string.Format("%1 Is Ready!", fac));
+		} else {
+			pOwnerEntity.SetYawPitchRoll("0 0 0");
+			SCR_HintManagerComponent.ShowCustomHint(string.Format("%1 Is Ready!", fac));
+		}
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override bool GetActionNameScript(out string outName)
+	{
+		string readyText;
+		if (this.ready) {
+			readyText = "Not Ready";
+		} else {
+			readyText = "Ready";
+		}
+		outName = (string.Format("Set Team as %1", readyText));
+		return true;
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override bool CanBePerformedScript(IEntity user)
+	{
+		return true;
+	}
+
+	override bool CanBroadcastScript()
+	{
+		return false;
+	}
+
+}
