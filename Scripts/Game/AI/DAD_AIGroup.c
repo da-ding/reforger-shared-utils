f9d657d0f902a92264124193f93170992dfced44
Da-ding! <109660283+da-ding@users.noreply.github.com>
2023-06-11 15:36:44 +1000

    call super in OnAgentAdded overrride

--- Scripts/Game/AI/DAD_AIGroup.c
+++ Scripts/Game/AI/DAD_AIGroup.c
@@ -2,6 +2,7 @@ modded class SCR_AIGroup
 {
 	override void OnAgentAdded(AIAgent child)
 	{
+		super.OnAgentAdded(child);
 		DAD_AIGroupProxyInfoComponent info = DAD_AIGroupProxyInfoComponent.Cast(child.FindComponent(DAD_AIGroupProxyInfoComponent));
 		if (info) info.UpdateInfo(this);
 	}
